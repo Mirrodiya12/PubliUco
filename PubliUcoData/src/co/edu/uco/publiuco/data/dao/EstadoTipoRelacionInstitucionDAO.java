55430dc4bfad8efb5f897c6c7d9deec774873cd0
Mirrodiya12 <125704692+Mirrodiya12@users.noreply.github.com>
2023-05-10 00:27:43 -0500

    Commit al día Data 10/05/2023

--- PubliUcoData/src/co/edu/uco/publiuco/data/dao/EstadoTipoRelacionInstitucionDAO.java
+++ PubliUcoData/src/co/edu/uco/publiuco/data/dao/EstadoTipoRelacionInstitucionDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import co.edu.uco.publiuco.entities.EstadoTipoRelacionInstitucionEntities;
 
-public interface EstadoTipoRelacionInstitucionDAO {
+public interface EstadoTipoRelacionInstitucionDAO{
 	
 	void create(EstadoTipoRelacionInstitucionEntities entities);
 	
